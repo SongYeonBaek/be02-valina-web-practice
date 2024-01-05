1e9f6d439f9561055f9b83e504db2bc6e486e789
baeksongyeon <songyeon0607@naver.com>
2024-01-05 09:20:34 +0900

    [Init] Vanlia Web 최초 Commit

---
+++ src/main/java/com/example/vanliaweb/VanliawebApplication.java
@@ -0,0 +1,13 @@
+package com.example.vanliaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanliawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanliawebApplication.class, args);
+	}
+
+}
